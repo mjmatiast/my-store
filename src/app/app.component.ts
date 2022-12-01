04620384ba0505c110d6eee39389da95cf673625
Matias <mmatiast@miumg.edu.gt>
2022-11-30 21:26:18 -0600

    practica de la 5 a la 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,102 @@
 import { Component } from '@angular/core';
 
+import {Product} from './models/product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = 'https://static.wikia.nocookie.net/dragonball/images/6/64/Final_Yamcha.png';
+  imgParent = ''; 
+  showImg = true;
   
-  onLoaded(){
-    console.log('log padre')
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '2',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '3',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '4',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '5',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '6',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '7',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '8',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '9',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '10',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '11',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '12',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+    {
+      id: '13',
+      name: 'Product 1',
+      image: './assets/images/r6.jpg',
+      price: 100
+    },
+  ];
+
+  onLoaded(img: string){
+    console.log("log padre", img)
+  }
+  toggleImg(){
+    this.showImg = !this.showImg;
   }
 }
 
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,22 +1,61 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} 
+from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  @Input('img') img: string = '';
+  img: string = '';
 
+  @Input('img')
+  set changeImg(newImg: string){
+    this.img = newImg;
+    console.log('change just new =>', this.img);
+    //code
+  }
   @Output() loaded = new EventEmitter();
 
-  imageDefault = '';
-   
-  constructor(){}
+  imageDefault = './assets/images/r6azul.jpg';
 
+  counter = 0;
+  counterFn: number | undefined
+   
+  constructor(){
+    //before reder, corremos una vez  cada vez angular quiere construir una instancia de ese componente
+    //No async
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges(){
+    //todos los imput se utlizan dentro de ngOnChange,  
+    //corre multiples antes o durante del rendereizacion simpre y cuando detecte cambios en los imputs.
+    //change inputs --times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+    
+  }
+  
   ngOnInit(): void {
-     
+    //corre antes de renderizar pero solo una vez cuando ya esta inicializado el componente, cosas async
+    //aqui podemos correr async --once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+    this.counterFn = window.setInterval(()=> {
+      this.counter += 1;
+      console.log('run counter');
+    }, 1000) 
+  }
+
+  ngAfterViewInit(){
+    //after render, corre cuando todos los hijos de ese componente ya se han renderizado 
+    //handler children
+    console.log('ngAfterViewInit');
+  }
+  ngOnDestroy() {
+    //corre cunado elimina este componente
+    console.log('ngOnDestroy');
+    window.clearInterval(this.counterFn);
   }
 
   imgError() {
